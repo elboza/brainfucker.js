280641828682f338846832ace7cc3525ba7f2e67
Fernando Iazeolla <fernando.iazeolla@gmail.com>
2015-10-05 02:35:39 +0200

    added run interpreter. TODO fix it.

--- bf.js
+++ bf.js
@@ -26,3 +26,68 @@ function pretty_bf(s){
 	r=x.match(/.{1,5}/g);
 	return r.join(' ');
 }
+function run(code,env){
+	ip=0;
+	ep=0;
+	if(env==null){
+		xenv=[0,0,0,0,0,0,0,0,0,0];
+	}
+	else{
+		xenv=env.split("");
+	}
+	output="";
+	while(ip<=code.length){
+		switch(code[ip]){
+			case '+':
+				xenv[ep]=String.fromCharCode(xenv[ep].charCodeAt(0)+1);
+				//xenv[ep]+=1;
+				break;
+			case '-':
+				xenv[ep]=String.fromCharCode(xenv[ep].charCodeAt(0)-1);
+				//xenv[ep]-=1;
+				break;
+			case '>':
+				++ep;if(ep>=env.length) ep=0;
+				break;
+			case '<':
+				--ep;if(ep<0) ep=env.length-1;
+				break;
+			case '.':
+				output+=xenv[ep];
+				break;
+			case ',':
+				xenv[ep]=prompt("input a character",0);
+				break;
+			case '[':
+				if(xenv[ep]==0){
+					nest=1;ip++;
+					while(nest){
+						if(code[ip]=='[') nest++;
+						if(code[ip]==']') nest--;
+						++ip;
+					}
+					ip-=2;
+				}
+				break;
+			case ']':
+				if(xenv[ep]!=0){
+					nest=1;ip--;
+					while(nest){
+						if(code[ip]==']') nest++;
+						if(code[ip]=='[') nest--;
+						--ip;
+					}
+					++ip;
+				}
+				break;
+			default:
+				
+		}
+		ip++;
+	}
+	env=xenv.join("");
+	return({"out":output,"env":env});
+}
+function out(){
+	alert("out");
+}
